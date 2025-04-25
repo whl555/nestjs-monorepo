4c33882775da18e0a1f9a29c21251112a0c0c46d
wuhaolei <nell53052@gmail.com>
2025-04-25 12:45:14 +0800

    import pipes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   logger.log(`Application is running on port: ${port}`);
--- apps/api/src/tasks/dto/create-task.dto.ts
+++ apps/api/src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,6 @@
+import { IsNotEmpty } from 'class-validator';
 export class CreateTaskDto {
-    title: string;
+    @IsNotEmpty() title: string;
     desc: string;
 }
+
